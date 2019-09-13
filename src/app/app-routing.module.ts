370d8fc199dc0234f2d791f9242df3c2682e5d33
Carlos Rodríguez <carlosdr9812@gmail.com>
2019-09-12 23:28:25 -0500

    certificado pdf

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,8 @@ import { LoginComponent } from './components/login/login.component';
 import { BienvenidoComponent } from './components/bienvenido/bienvenido.component';
 import { CargaHomeComponent } from './components/carga-home/carga-home.component';
 import { BienvenidoRojoComponent } from './components/bienvenido-rojo/bienvenido-rojo.component';
+import { CertificadoComponent } from './components/certificado/certificado.component';
+
 
 
 
@@ -20,7 +22,8 @@ const routes: Routes = [
   {path:"login",component:LoginComponent },
   {path:"bienvenido",component:BienvenidoComponent },
   {path:"home",component:CargaHomeComponent},
-  {path:"bienvenidorojo",component:BienvenidoRojoComponent},  
+  {path:"bienvenidorojo",component:BienvenidoRojoComponent},
+  {path:"certificado",component:CertificadoComponent},  
 ];
 
 @NgModule({

ff213b8196d967c68eaeb39057dca0282758944a
unknown <santiagolope373@gmail.com>
2019-09-18 13:56:27 -0500

    metodos offline-santiago

--- src/app/components/escoger/escoger.component.ts
+++ src/app/components/escoger/escoger.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {GeneralServiceService}from 'src/app/services/general-service.service';
 import { forkJoin } from 'rxjs';
 import { Router } from '@angular/router';
+import {SincronizacionService} from 'src/app/services/sincronizacion.service'; 
 
 @Component({
   selector: 'app-escoger',
@@ -12,7 +13,7 @@ export class EscogerComponent implements OnInit {
 
 	encuestas: any[];
 
-  constructor(private generalService: GeneralServiceService, private router: Router) { }
+  constructor(private generalService: GeneralServiceService, private router: Router,private sincronizacionServicio: SincronizacionService) { }
 
   ngOnInit() {
   	let filter = {};
@@ -25,21 +26,27 @@ export class EscogerComponent implements OnInit {
   goColor(color:string){
   	var encuesta = this.encuestas.find(enc=>{
   		return enc.tipo == color;
-  	});
-  	if(encuesta){
-  		var data = {
-			  id_usuario:window.localStorage.id_usuario,
-			  id_localizacion:window.localStorage.id_localizacion,
-	  		fecha: new Date(),
-	  		id_encuesta:encuesta.id
-	  	};
-	  	window.localStorage.setItem("id_encuesta",encuesta.id);
+	  });
+	  
+
+		if(encuesta){
+			var data = {
+				id_usuario:window.localStorage.id_usuario,
+				id_localizacion:window.localStorage.id_localizacion,
+				fecha: new Date(),
+				id_encuesta:encuesta.id
+			};	
+	  
+  	
+		  window.localStorage.setItem("id_encuesta",encuesta.id);
+		  console.log ('offline')
 	  	/*console.log(data);*/
 	  	this.generalService.postEntrada(data).subscribe(res=>{
 	  		console.log(res);
 	  		window.localStorage.setItem("id_entrada",res.id);
 	  		this.router.navigate(['/encuesta'+color]);
-	  	});
+		  });
+		
   	}else{
   		alert("Error! No se encuentra encuesta "+color+", contacta soporte!");
   	}
@@ -52,6 +59,27 @@ export class EscogerComponent implements OnInit {
 
   goRed(){
   	this.goColor("roja");
+  }
+
+  testCrear(){
+    console.log("Creando...");
+    var elNuevo = {id_pregunta:1,id_item:2,respuesta:"si",id_entrada:1,dexie:true};
+    this.sincronizacionServicio.addUsuario(elNuevo);
+
+  }
+
+  testEditar(){
+    console.log("Editando...");
+    var elViejo = {id:1, id_pregunta:1,id_item:2,respuesta:"si pero talvez",id_entrada:1,dexie:true};
+    this.sincronizacionServicio.putUsuario(elViejo);
+    
+  }
+
+  testEliminar(){
+    console.log("Eliminando...");
+    var elViejo = {id:1, id_pregunta:1,id_item:2,respuesta:"si pero talvez",id_entrada:1,dexie:true};
+    this.sincronizacionServicio.deleteUsuario(elViejo.id);
+    
   }
 
 }
--- src/app/services/sincronizacion.service.ts
+++ src/app/services/sincronizacion.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { ServicioOfflineService } from './servicio-offline.service';
 import { Router, ActivatedRoute, Route } from "@angular/router";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import Dexie from 'dexie';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +11,7 @@ export class SincronizacionService {
 
   constructor() { }
 
-  private registerToEvents(servicioOfflineService: ServicioOfflineService) {
+  private registerToEvents(servicioOfflineService: ServicioOfflineService,httpClient: HttpClient) {
     
     servicioOfflineService.connectionChanged.subscribe(online => {
     
@@ -25,6 +27,138 @@ export class SincronizacionService {
        
       }
     });
+
+    this.crearTablas();
     
   }
+
+  private usuariosDb: any;
+   private entradasDb: any;
+   private respuestasDb: any;
+ 
+   private crearTablas(){
+     //TABLA USUARIOS
+     //Hacer new Dexie
+     this.usuariosDb = new Dexie("TablaUsuarios");
+     //Hacer crear la tabla (bases datos)
+     this.usuariosDb.version(1).stores({
+       usuarios: "++id,nombre,cedula,id_localizacion"
+     });
+     //Verificar que quedo bien
+     this.usuariosDb.open().catch(function (err) {
+       console.error (err.stack || err);
+     });
+ 
+     //TABLA ENTRADAS
+     //Hacer new Dexie
+     this.entradasDb = new Dexie("TablaEntradas");
+     //Hacer crear la tabla (bases datos)
+     this.entradasDb.version(1).stores({
+       entradas: "++id,id_encuesta,fecha,id_usuario,dexie"
+     });
+     //Verificar que quedo bien
+     this.entradasDb.open().catch(function (err) {
+       console.error (err.stack || err);
+     });
+ 
+     //TABLA RESPUESTAS
+     //Hacer new Dexie
+     this.respuestasDb = new Dexie("TablaRespuestas");
+     //Hacer crear la tabla (bases datos)
+     this.respuestasDb.version(1).stores({
+       usuarios: "++id,id_pregunta,id_item,respuesta,id_entrada,dexie"
+     });
+     //Verificar que quedo bien
+     this.respuestasDb.open().catch(function (err) {
+       console.error (err.stack || err);
+     });
+ 
+   }
+ 
+ 
+   async addUsuario(usuario){
+     /*usuario--> {id_pregunta:1,id_item:2,respuesta:"si",id_entrada:1,dexie:true};*/
+     this.usuariosDb.usuarios.add(usuario).then(function(res){
+       console.log(res);
+     }).catch(e=>{
+       alert("Error de dexie");
+     });
+   }
+ 
+   async putUsuario(usuario){
+     /*usuario--> {id:1,id_pregunta:1,id_item:2,respuesta:"si",id_entrada:1,dexie:true};*/
+     this.usuariosDb.usuarios.put(usuario).then(function(res){
+       console.log(res);
+     }).catch(e=>{
+       alert("Error de dexie");
+     });
+   }
+ 
+   async deleteUsuario(id_usuario){
+     /*usuario--> {id:1,id_pregunta:1,id_item:2,respuesta:"si",id_entrada:1,dexie:true};*/
+     this.usuariosDb.usuarios.delete(id_usuario).then(function(res){
+       console.log(res);
+     }).catch(e=>{
+       alert("Error de dexie");
+     });
+   }
+
+   async addEntrada(entrada){
+    /*entrada-> {id_pregunta:1,id_item:2,respuesta:"si",id_entrada:1,dexie:true};*/
+    this.entradasDb.entradas.add(entrada).then(function(res){
+      console.log(res);
+    }).catch(e=>{
+      alert("Error de dexie");
+    });
+  }
+
+  async putEntrada(entrada){
+    /*entrada-> {id_pregunta:1,id_item:2,respuesta:"si",id_entrada:1,dexie:true};*/
+    this.entradasDb.entradas.put(entrada).then(function(res){
+      console.log(res);
+    }).catch(e=>{
+      alert("Error de dexie");
+    });
+  }
+
+  async deleteEntrada(id_Entrada){
+    /*entrada-> {id_pregunta:1,id_item:2,respuesta:"si",id_entrada:1,dexie:true};*/
+    this.entradasDb.entradas.delete(id_Entrada).then(function(res){
+      console.log(res);
+    }).catch(e=>{
+      alert("Error de dexie");
+    });
+  }
+
+  async addRespuesta(respuesta){
+    /*Respuesta--> {id_pregunta:1,id_item:2,respuesta:"si",id_entrada:1,dexie:true};*/
+    this.respuestasDb.respuestas.add(respuesta).then(function(res){
+      console.log(res);
+    }).catch(e=>{
+      alert("Error de dexie");
+    });
+  }
+
+  async putRespuesta(respuesta){
+    /*Respuesta--> {id_pregunta:1,id_item:2,respuesta:"si",id_entrada:1,dexie:true};*/
+    this.respuestasDb.respuestas.put(respuesta).then(function(res){
+      console.log(res);
+    }).catch(e=>{
+      alert("Error de dexie");
+    });
+  }
+
+  async deleteRespuesta(id_Respuesta){
+    /*Respuesta--> {id_pregunta:1,id_item:2,respuesta:"si",id_entrada:1,dexie:true};*/
+    this.respuestasDb.respuestas.delete(id_Respuesta).then(function(res){
+      console.log(res);
+    }).catch(e=>{
+      alert("Error de dexie");
+    });
+  }
+
+
+
+
+
 }
